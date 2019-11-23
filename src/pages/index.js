3fe53ed0f988dce6d646a91e7232b266e9e96ff2
Dalton Saffe <daltonsaffe@gmail.com>
2019-11-22 22:40:55 -0500

    init site

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <section>
+      <Link to="/page-2/">dev</Link>
+      <Link to="/page-2/">photos</Link>
+      <Link to="/page-2/">music</Link>
+    </section>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
