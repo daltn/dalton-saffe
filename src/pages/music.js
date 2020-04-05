4051167747d31308a734c76b2157af04d6bb82c4
Dalton Saffe <daltonsaffe@gmail.com>
2020-04-05 13:01:45 -0400

    add mix

--- src/pages/music.js
+++ src/pages/music.js
@@ -9,6 +9,14 @@ const MusicPage = () => (
     <h1>music</h1>
     <h3>mixes</h3>
     <iframe
+      width="100%"
+      height="166"
+      scrolling="no"
+      frameBorder="no"
+      allow="autoplay"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/791437048&color=%23272728&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    ></iframe>
+    <iframe
       title="Dalton Saffe - Ambient 02"
       width="100%"
       height="166"
